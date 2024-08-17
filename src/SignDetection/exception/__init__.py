56fa2ace97a6c554a42cd71be79feedb7f49701d
pramodkhavare <pramodkhavare2000@gmail.com>
2024-08-17 13:21:59 +0530

    Exception-Logger Added

--- src/SignDetection/exception/__init__.py
+++ src/SignDetection/exception/__init__.py
@@ -1,24 +1,32 @@
-import sys
-from src.SignDetection.logger import logging
-
-def error_message_detail(error,error_detail:sys):
-    _,_,exc_tb = error_detail.exc_info()
-    file_name = exc_tb.tb_frame.f_code.co_filename
-
-    error_message = "Error occured in python script name [{0}] line number [{1}] error message [{2}]".format(
-        file_name, exc_tb.tb_lineno, str(error)
-    )
-
-    return error_message
+import os , sys 
 
 class CustomException(Exception):
+    def __init__(self ,error_message:Exception ,error_details: sys):
+        self.error_message =CustomException.get_detail_error_message(error_message=error_message ,error_details=error_details)
+        
     
-    def __init__(self, error_message, error_detail:sys):
-        super().__init__(error_message)
-        self.error_message = error_message_detail(error_message, error_detail=error_detail)
-
+    @staticmethod
+    def get_detail_error_message(error_message:Exception ,error_details: sys)->str:
+        _, _, exce_tb =error_details.exc_info()
+        
+        exception_block_line_number = exce_tb.tb_frame.f_lineno
+        try_block_line_number = exce_tb.tb_lineno
+        filename =exce_tb.tb_frame.f_code.co_filename
+        
+        error_message = f"""
+        
+        Error occured in execution of :
+        [{filename}] at 
+        try block line number :[{try_block_line_number}]
+        and exception block line number : [{exception_block_line_number}]
+        error message : [{error_message}]
+        
+        """
+        return error_message
+    
+    
     def __str__(self):
-        return self.error_message    
+        return self.error_message
     
-
-
+    def __repr__(self):
+        return CustomException.__name__.str()
